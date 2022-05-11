41209f4f50a719787f1e2be17e33f4dddd724307
gabitodev <gabitodev@gmail.com>
2022-05-11 18:38:41 -0400

    now works with front end

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
+app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 
